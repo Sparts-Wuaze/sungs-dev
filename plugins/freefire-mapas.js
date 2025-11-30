3f1d6dc086c29962c30bbf0aacf8dc97c899d61d
felixdev <sungasfa@gmail.com>
2025-11-29 21:03:48 -0400

    Actualizar freefire-mapas.js

--- plugins/freefire-mapas.js
+++ plugins/freefire-mapas.js
@@ -1,13 +1,29 @@
-const handler = async (m, { conn }) => {
-  const url = 'https://files.catbox.moe/3sf8cz.jpg'
-  await conn.sendMessage(m.chat, {
-    image: { url },
-    caption: '*KALAHARI*'
-  }, { quoted: m })
+const mapImages = {
+  kalahari: 'https://files.catbox.moe/3sf8cz.jpg',      // Kalahari (original intacta)
+  alpes:    'https://files.catbox.moe/b09zpc.jpg',      // Alpes
+  bermuda:  'https://files.catbox.moe/3c5xp3.jpg',      // Bermuda
+  purgatorio:'https://files.catbox.moe/u87o1o.jpg'      // Purgatorio
 }
 
-handler.command = /^kalahari$/i
-handler.help = ['kalahari']
+const mapNames = {
+  kalahari:   '*KALAHARI*',
+  alpes:      '*ALPES*',
+  bermuda:    '*BERMUDA*',
+  purgatorio: '*PURGATORIO*'
+}
+
+const handler = async (m, { conn, command }) => {
+  let mapaKey = command.toLowerCase()
+  if (mapImages[mapaKey]) {
+    await conn.sendMessage(m.chat, {
+      image: { url: mapImages[mapaKey] },
+      caption: mapNames[mapaKey]
+    }, { quoted: m })
+  }
+}
+
+handler.help = Object.keys(mapImages)
 handler.tags = ['media']
+handler.command = /^(kalahari|alpes|bermuda|purgatorio)$/i
 
 export default handler
